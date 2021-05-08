e95e58ad0822ddfbc41af8ade4f1066033fcff78
evankender <evankender503@gmail.com>
2021-05-07 22:43:15 -0700

    api scrape added

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('link');
-    // onClick's logic below:
     link.addEventListener('click', function() {
-        console.log("x");
+      chrome.tabs.query({active:true,currentWindow:true},function(tab){
+
+        var jsonURL = 'http://0.0.0.0:5000/getPageData?url=' + tab[0].url;
+        fetch(jsonURL)
+        .then(res => res.json())
+        .then((out) => {
+          console.log('Checkout this JSON! ', out);
+        })
+        .catch(err => { throw err });
+
+      });
     });
 });
--- Scraper/amazon.py
+++ Scraper/amazon.py
@@ -7,20 +7,19 @@ from flask import Flask, redirect, url_for, request, render_template
 import logging
 import os
 from flask_restful import Resource, Api
-from bson import json_util
 
 app = flask.Flask(__name__)
 api = Api(app)
 
-e = Extractor.from_yaml_file('selectors.yml')
+amazonProduct = Extractor.from_yaml_file('amazonProduct.yml')
 
 class getPageData(Resource):
     def get(self, type="json"):
         if request.args.get('url') is not None:
-            app.logger.info(request.args.get('url'))
+            return flask.make_response(scrape(request.args.get('url')))
 
-api.add_resource(listAll, '/url')
-api.add_resource(listAll, '/url/<type>', endpoint="url")
+api.add_resource(getPageData, '/getPageData')
+api.add_resource(getPageData, '/getPageData/<type>', endpoint="getPageData")
 
 def scrape(url):
     headers = {
@@ -29,7 +28,7 @@ def scrape(url):
         'cache-control': 'no-cache',
         'dnt': '1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Ch0.0.0rome/51.0.2704.64 Safari/537.36',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'sec-fetch-site': 'none',
         'sec-fetch-mode': 'navigate',
@@ -44,7 +43,7 @@ def scrape(url):
     if r.status_code > 500:
         return None
     # Pass the HTML of the page and create
-    return e.extract(r.text)
+    return amazonProduct.extract(r.text)
 
 
 if __name__ == "__main__":
